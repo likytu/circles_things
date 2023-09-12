c7e7602e24aa603d660e6bc29b1f3a49050cb752
Blake O'Callahan <allowed401@gmail.com>
2023-09-12 21:19:47 +1200

    settings visual glitch patch

--- settings.lua
+++ settings.lua
@@ -3,31 +3,27 @@ dofile("mods/circles_things/files/scripts/lib/all_things_scaling.lua")
 local amount = tonumber(ModSettingGet("circles_things.amount")) or 0
 local period = tonumber(ModSettingGet("circles_things.period")) or 1
 local difficulty = get_difficulty(amount, period)
+local max_period = 30
 function mod_setting_scaling_change_callback( mod_id, gui, in_main_menu, setting, old_value, new_value  )
-	local max_period = 30
+	if new_value == "" then return end
 	if setting.id == "period" then
 		period = tonumber(new_value)
-		if period == nil then
-			period = 1
-			return
-		end
 		if period > max_period then
+			ModSettingSet("circles_things.period", max_period)
 			period = max_period
-			ModSettingSet("circles_things.period", "30")
 		end
 	end
 	if setting.id == "amount" then
 		amount = tonumber(new_value)
-		if amount == nil then
-			amount = 10
-			return
-		end
 	end
 	difficulty = get_difficulty(amount, period)
-	mod_settings[1].settings[3].ui_name = "Difficulty Rating: "..difficulty
 	print(amount, period, difficulty )
 end
 
+function ui_fn_difficulty(mod_id, gui, in_main_menu, im_id, setting)
+	local text = "Difficulty Rating: "..difficulty
+	GuiText(gui, mod_setting_group_x_offset, 0, text)
+end
 local mod_id = "circles_things" -- This should match the name of your mod's folder.
 mod_settings_version = 1 -- This is a magic global that can be used to migrate settings to new mod versions. call mod_settings_get_version() before mod_settings_update() to get the old value. 
 mod_settings = 
@@ -40,7 +36,7 @@ mod_settings =
 			{
 				id = "period",
 				ui_name = "Period",
-				ui_description = "How many minutes between each effect, if above 30, set to 30",
+				ui_description = "How many minutes between each effect, if above "..max_period..", set to "..max_period,
 				value_default = "1",
 				text_max_length = 20,
 				allowed_characters = ".0123456789",
@@ -59,8 +55,7 @@ mod_settings =
 			},
 			{
 				id = "difficulty_rating",
-				ui_name = "Difficulty Rating: "..difficulty,
-				not_setting = true,
+				ui_fn=ui_fn_difficulty,
 			},
 		},
 	},
