133db2938f4cd7e93c792bf89b406ca570ca1d1d
Blake O'Callahan <allowed401@gmail.com>
2023-09-12 16:54:22 +1200

    tweaked scaling

--- files/scripts/lib/all_things_scaling.lua
+++ files/scripts/lib/all_things_scaling.lua
@@ -5,5 +5,5 @@ function get_scaling_effect_hp (newgame_n, scaling_effect)
     return (0.8 + 2*math.pow(newgame_n, 2)) * scaling_effect
 end
 function get_scaling_effect_attack_speed (newgame_n, scaling_effect)
-    return (math.pow( 0.5, newgame_n )) / scaling_effect
+    return (math.pow( 0.5, newgame_n )) / scaling_effect * 1.3
 end
